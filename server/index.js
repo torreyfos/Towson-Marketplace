a140d893e64c171d95bbcb1078fd0a5c63e70408
Torrey <torreyfos@gmail.com>
2026-03-18 12:05:09 -0400

    connected to MongoDB Atlas

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+//Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('Connected to MongoDB Atlas!'))
+    .catch((err) => console.log('MongoDB connection error:', err));
 
 app.use(cors());
 app.use(express.json());
