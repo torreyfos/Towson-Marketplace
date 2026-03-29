9d553a531e894002af32f9e4532651d5f24ce393
Taylor Williams <nuenue416@gmail.com>
2026-03-29 00:37:57 -0400

    Successfully connected server to MongoDB database

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config({debug: true });
+
 const mongoose = require('mongoose');
-require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
