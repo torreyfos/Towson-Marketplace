96d9890455674f67bec367172d7fbfa4e93c602c
Beatrice Amoako-Dua <amoakob795@gmail.com>
2026-04-21 19:09:53 -0400

    Update index.js
    User rating functionality

--- server/index.js
+++ server/index.js
@@ -14,8 +14,45 @@ mongoose.connect(process.env.MONGO_URI)
 app.use(cors());
 app.use(express.json());
 
+const User = mongoose.model("User", {
+    email: String,
+    ratings: [
+        {
+            rating: Number,
+            fromUser: String
+        }
+    ]
+});
+
 app.get('/', (req, res) => {
     res.send('TU Marketplace server is running!');
+});
+
+app.post("/rate-user", async (req, res) => {
+    try {
+        const { sellerEmail, rating, fromUser } = req.body;
+
+        let user = await User.findOne({ email: sellerEmail });
+
+        if (!user) {
+            user = new User({
+                email: sellerEmail,
+                ratings: []
+            });
+        }
+
+        user.ratings.push({
+            rating: Number(rating),
+            fromUser
+        });
+
+        await user.save();
+
+        res.json({ message: "Rating saved successfully" });
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
 app.listen(PORT, () => {
